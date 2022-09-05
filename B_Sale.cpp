7bd436755f711bf180554fabf51268ceaaf5d843
Kapil Soni <kapilsoni54768161@gmail.com>
2022-09-05 20:35:37 +0530

    .

---
+++ B_Sale.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    int ans = 0;
+    for (int i = 0; i < m; i++)
+    {
+        if (v[i] > 0)
+            break;
+        ans += abs(v[i]);
+    }
+    cout << ans << endl;
+    return 0;
+}
