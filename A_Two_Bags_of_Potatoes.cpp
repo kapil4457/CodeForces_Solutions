b65a4ac487a82fb063dba73a68c940595bf16001
Kapil Soni <kapilsoni54768161@gmail.com>
2022-09-14 22:33:27 +0530

    .

---
+++ A_Two_Bags_of_Potatoes.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    int y, k, n;
+    scanf("%d%d%d", &y, &k, &n);
+    int x = k - y % k;
+    int top = n - y;
+    if (x <= top)
+    {
+        printf("%d", x);
+        x += k;
+        while (x <= top)
+        {
+            printf(" %d", x);
+            x += k;
+        }
+        printf("\n");
+    }
+    else
+    {
+        printf("-1\n");
+    }
+    return 0;
+}
