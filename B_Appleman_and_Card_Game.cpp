8b096fd2afd4c8e33190873839256934ca56bc4e
Kapil Soni <kapilsoni54768161@gmail.com>
2022-09-22 22:26:02 +0530

    .

--- B_Appleman_and_Card_Game.cpp
+++ B_Appleman_and_Card_Game.cpp
@@ -1,3 +1,5 @@
+// Method 1 :
+
 #include <bits/stdc++.h>
 #define ll long long int
 #define pb push_back
@@ -64,5 +66,39 @@ int main()
 
     cout << answer << endl;
 
+    return 0;
+}
+
+// Method 2 :
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+typedef long long LL;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    LL n, m, ans = 0;
+    LL c[27] = {};
+    string s;
+    cin >> n >> m;
+    cin >> s;
+    for (int i = 0; i < n; ++i)
+        c[s[i] - 'A']++;
+    sort(c, c + 27);
+    for (int i = 26; i >= 0 && m; --i)
+    {
+        if (m >= c[i])
+        {
+            m -= c[i];
+            ans += c[i] * c[i];
+        }
+        else
+        {
+            ans += m * m;
+            break;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
