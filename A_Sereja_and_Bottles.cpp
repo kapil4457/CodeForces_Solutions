cf2807c52fd9a88fb1d5645670b7cb832c3ba8fc
Kapil Soni <kapilsoni54768161@gmail.com>
2022-09-23 10:59:11 +0530

    .

---
+++ A_Sereja_and_Bottles.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007ll // 998244353ll
+#define mii map<int, int>
+#define pii pair<int, int>
+using namespace std;
+int main()
+{
+    int no_of_bottles;
+    scanf("%d", &no_of_bottles);
+
+    vector<int> brand(no_of_bottles + 1, 0);
+    vector<int> can_open(no_of_bottles + 1, 0);
+
+    for (int i = 1; i <= no_of_bottles; i++)
+        scanf("%d %d", &brand[i], &can_open[i]);
+
+    int impossible_bottles = 0;
+    for (int i = 1; i <= no_of_bottles; i++)
+    {
+        int openable = false;
+
+        for (int j = 1; j <= no_of_bottles; j++)
+        {
+            if (can_open[j] == brand[i] && i != j)
+                openable = true;
+        }
+
+        impossible_bottles += (!openable);
+    }
+
+    printf("%d\n", impossible_bottles);
+    return 0;
+}
