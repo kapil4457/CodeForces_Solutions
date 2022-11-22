2ce0b7cbe7672cd3dda5b338bea1c840ee14b7fc
Kapil Soni <kapilsoni54768161@gmail.com>
2022-11-22 15:22:07 +0530

    .

---
+++ A_C.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007ll // 998244353ll
+#define mii map<int, int>
+#define pii pair<int, int>
+int power(int a, int b)
+{
+    int result = 1;
+    while (b > 0)
+    {
+        if (b % 2)
+            result *= a;
+        a *= a;
+        b /= 2;
+    }
+    return result;
+}
+using namespace std;
+int main()
+{
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        ll a = 5, b = 4, n = 100;
+        cin >> a >> b >> n;
+        ll count = 0;
+        while (a <= n || b <= n)
+        {
+            if (a < b)
+            {
+                a += b;
+            }
+            else
+            {
+                b += a;
+            }
+            count++;
+        }
+
+        cout << count - 1 << endl;
+    }
+    return 0;
+}
