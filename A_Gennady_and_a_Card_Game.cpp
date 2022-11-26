b73a692a5349a805a76a5cd35b1387001b1eab87
Kapil Soni <kapilsoni54768161@gmail.com>
2022-11-26 16:58:38 +0530

    .

---
+++ A_Gennady_and_a_Card_Game.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007ll // 998244353ll
+#define mii map<int, int>
+#define pii pair<int, int>
+int power(int a, int b)
+{
+    int result = 1;
+    while (b > 0)
+    {
+        if (b % 2)
+            result *= a;
+        a *= a;
+        b /= 2;
+    }
+    return result;
+}
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    vector<string> v(5);
+
+    for (ll i = 0; i < 5; i++)
+    {
+        cin >> v[i];
+    }
+
+    bool check = false;
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (s[0] == v[i][0])
+        {
+            check = true;
+            break;
+        }
+    }
+
+    if (check)
+    {
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        if (s[1] == v[i][1])
+        {
+            check = true;
+            break;
+        }
+    }
+
+    if (check)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+    return 0;
+}
