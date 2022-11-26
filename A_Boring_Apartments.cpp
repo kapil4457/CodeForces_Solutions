d1af9c3e5741de2f541f921452180426632660f9
Kapil Soni <kapilsoni54768161@gmail.com>
2022-11-26 17:22:53 +0530

    .

---
+++ A_Boring_Apartments.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define mod 1000000007ll // 998244353ll
+#define mii map<int, int>
+#define pii pair<int, int>
+int power(int a, int b)
+{
+    int result = 1;
+    while (b > 0)
+    {
+        if (b % 2)
+            result *= a;
+        a *= a;
+        b /= 2;
+    }
+    return result;
+}
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string str;
+        cin >> str;
+
+        char temp = str[0];
+
+        int sets = str[0] - '1';
+
+        int ans = sets * 10;
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            ans += i + 1;
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
